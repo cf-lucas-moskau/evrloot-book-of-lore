76e8e3e2e4197d6316e31f38c6ea8fa724b5dcae
Steven Pineda <steven2308@outlook.com>
2024-11-23 13:24:01 -0500

    Generates all needed metadata.

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,5 +1,125 @@
 export const BENEFICIARY_ADDRESS = '0x000000000000000000000000000000000000dead'; // TODO: update
 
-const BASE_URI = 'ipfs://TODO';
+const BASE_URI = 'ipfs://QmcJBhzfB3pMhZTKNBtax9xp6Sb5xrvc2aNgePmM3VQpxd';
 export const BOOK_METADATA_URL = `${BASE_URI}/book/collection.json`;
 export const PAGE_METADATA_URL = `${BASE_URI}/page/collection.json`;
+
+// For child NFTS
+export const PAGE_1_METADATA_URI = `${BASE_URI}/page_1.json`;
+export const PAGE_2_METADATA_URI = `${BASE_URI}/page_2.json`;
+export const PAGE_3_METADATA_URI = `${BASE_URI}/page_3.json`;
+export const PAGE_4_METADATA_URI = `${BASE_URI}/page_4.json`;
+export const PAGE_5_METADATA_URI = `${BASE_URI}/page_5.json`;
+export const PAGE_6_METADATA_URI = `${BASE_URI}/page_6.json`;
+export const PAGE_7_METADATA_URI = `${BASE_URI}/page_7.json`;
+export const PAGE_8_METADATA_URI = `${BASE_URI}/page_8.json`;
+export const PAGE_9_METADATA_URI = `${BASE_URI}/page_9.json`;
+export const PAGE_10_METADATA_URI = `${BASE_URI}/page_10.json`;
+
+// Fixed Part Metadata
+export const FIXED_PART_BOOK_BASE_METADATA_URI = `${BASE_URI}/catalog/fixed/book_base.json`;
+export const FIXED_PART_FALLBACK_RUNE_1_METADATA_URI = `${BASE_URI}/catalog/fixed/fallback_rune_1.json`;
+export const FIXED_PART_FALLBACK_RUNE_2_METADATA_URI = `${BASE_URI}/catalog/fixed/fallback_rune_2.json`;
+export const FIXED_PART_FALLBACK_RUNE_3_METADATA_URI = `${BASE_URI}/catalog/fixed/fallback_rune_3.json`;
+export const FIXED_PART_FALLBACK_TREE_METADATA_URI = `${BASE_URI}/catalog/fixed/fallback_tree.json`;
+export const FIXED_PART_RIBBON_BLUE_METADATA_URI = `${BASE_URI}/catalog/fixed/ribbon_blue.json`;
+export const FIXED_PART_RIBBON_GREEN_METADATA_URI = `${BASE_URI}/catalog/fixed/ribbon_green.json`;
+export const FIXED_PART_RIBBON_RED_METADATA_URI = `${BASE_URI}/catalog/fixed/ribbon_red.json`;
+export const FIXED_PART_RIBBON_PURPLE_METADATA_URI = `${BASE_URI}/catalog/fixed/ribbon_purple.json`;
+export const FIXED_PART_RIBBON_SHADOW_METADATA_URI = `${BASE_URI}/catalog/fixed/ribbon_shadow.json`;
+export const FIXED_PART_RUNE_1A_METADATA_URI = `${BASE_URI}/catalog/fixed/rune_1a.json`;
+export const FIXED_PART_RUNE_1B_METADATA_URI = `${BASE_URI}/catalog/fixed/rune_1b.json`;
+export const FIXED_PART_RUNE_1C_METADATA_URI = `${BASE_URI}/catalog/fixed/rune_1c.json`;
+export const FIXED_PART_RUNE_2A_METADATA_URI = `${BASE_URI}/catalog/fixed/rune_2a.json`;
+export const FIXED_PART_RUNE_2B_METADATA_URI = `${BASE_URI}/catalog/fixed/rune_2b.json`;
+export const FIXED_PART_RUNE_2C_METADATA_URI = `${BASE_URI}/catalog/fixed/rune_2c.json`;
+export const FIXED_PART_RUNE_3A_METADATA_URI = `${BASE_URI}/catalog/fixed/rune_3a.json`;
+export const FIXED_PART_RUNE_3B_METADATA_URI = `${BASE_URI}/catalog/fixed/rune_3b.json`;
+export const FIXED_PART_RUNE_3C_METADATA_URI = `${BASE_URI}/catalog/fixed/rune_3c.json`;
+export const FIXED_PART_TREE_BLUE_METADATA_URI = `${BASE_URI}/catalog/fixed/tree_blue.json`;
+export const FIXED_PART_TREE_BRIGHT_METADATA_URI = `${BASE_URI}/catalog/fixed/tree_bright.json`;
+export const FIXED_PART_TREE_DARK_METADATA_URI = `${BASE_URI}/catalog/fixed/tree_dark.json`;
+
+// Slot Part Metadata
+export const SLOT_PART_PAGE_1_METADATA_URI = `${BASE_URI}/catalog/slot/page_1.json`;
+export const SLOT_PART_PAGE_2_METADATA_URI = `${BASE_URI}/catalog/slot/page_2.json`;
+export const SLOT_PART_PAGE_3_METADATA_URI = `${BASE_URI}/catalog/slot/page_3.json`;
+export const SLOT_PART_PAGE_4_METADATA_URI = `${BASE_URI}/catalog/slot/page_4.json`;
+export const SLOT_PART_PAGE_5_METADATA_URI = `${BASE_URI}/catalog/slot/page_5.json`;
+export const SLOT_PART_PAGE_6_METADATA_URI = `${BASE_URI}/catalog/slot/page_6.json`;
+export const SLOT_PART_PAGE_7_METADATA_URI = `${BASE_URI}/catalog/slot/page_7.json`;
+export const SLOT_PART_PAGE_8_METADATA_URI = `${BASE_URI}/catalog/slot/page_8.json`;
+export const SLOT_PART_PAGE_9_METADATA_URI = `${BASE_URI}/catalog/slot/page_9.json`;
+export const SLOT_PART_PAGE_10_METADATA_URI = `${BASE_URI}/catalog/slot/page_10.json`;
+
+// Z-Indices
+export const FIXED_PART_BOOK_BASE_Z_INDEX = 1;
+export const FIXED_PART_RIBBON_BLUE_Z_INDEX = 2;
+export const FIXED_PART_RIBBON_GREEN_Z_INDEX = 2;
+export const FIXED_PART_RIBBON_RED_Z_INDEX = 2;
+export const FIXED_PART_RIBBON_PURPLE_Z_INDEX = 2;
+export const FIXED_PART_RIBBON_SHADOW_Z_INDEX = 2;
+
+export const FIXED_PART_FALLBACK_RUNE_1_Z_INDEX = 7;
+export const FIXED_PART_FALLBACK_RUNE_2_Z_INDEX = 6;
+export const FIXED_PART_FALLBACK_RUNE_3_Z_INDEX = 5;
+export const FIXED_PART_FALLBACK_TREE_Z_INDEX = 4;
+
+export const FIXED_PART_RUNE_1A_Z_INDEX = 3;
+export const FIXED_PART_RUNE_1B_Z_INDEX = 3;
+export const FIXED_PART_RUNE_1C_Z_INDEX = 3;
+export const FIXED_PART_RUNE_2A_Z_INDEX = 3;
+export const FIXED_PART_RUNE_2B_Z_INDEX = 3;
+export const FIXED_PART_RUNE_2C_Z_INDEX = 3;
+export const FIXED_PART_RUNE_3A_Z_INDEX = 3;
+export const FIXED_PART_RUNE_3B_Z_INDEX = 3;
+export const FIXED_PART_RUNE_3C_Z_INDEX = 3;
+export const FIXED_PART_TREE_BLUE_Z_INDEX = 3;
+export const FIXED_PART_TREE_BRIGHT_Z_INDEX = 3;
+export const FIXED_PART_TREE_DARK_Z_INDEX = 3;
+
+export const SLOT_PART_PAGE_1_Z_INDEX = 8;
+export const SLOT_PART_PAGE_2_Z_INDEX = 9;
+export const SLOT_PART_PAGE_3_Z_INDEX = 10;
+export const SLOT_PART_PAGE_4_Z_INDEX = 11;
+export const SLOT_PART_PAGE_5_Z_INDEX = 12;
+export const SLOT_PART_PAGE_6_Z_INDEX = 13;
+export const SLOT_PART_PAGE_7_Z_INDEX = 14;
+export const SLOT_PART_PAGE_8_Z_INDEX = 15;
+export const SLOT_PART_PAGE_9_Z_INDEX = 16;
+export const SLOT_PART_PAGE_10_Z_INDEX = 17;
+
+// Part IDs
+export const FIXED_PART_BOOK_BASE_PART_ID = 1;
+export const FIXED_PART_FALLBACK_RUNE_1_PART_ID = 2;
+export const FIXED_PART_FALLBACK_RUNE_2_PART_ID = 3;
+export const FIXED_PART_FALLBACK_RUNE_3_PART_ID = 4;
+export const FIXED_PART_FALLBACK_TREE_PART_ID = 5;
+export const FIXED_PART_RIBBON_BLUE_PART_ID = 6;
+export const FIXED_PART_RIBBON_GREEN_PART_ID = 7;
+export const FIXED_PART_RIBBON_RED_PART_ID = 8;
+export const FIXED_PART_RIBBON_PURPLE_PART_ID = 9;
+export const FIXED_PART_RIBBON_SHADOW_PART_ID = 10;
+export const FIXED_PART_RUNE_1A_PART_ID = 11;
+export const FIXED_PART_RUNE_1B_PART_ID = 12;
+export const FIXED_PART_RUNE_1C_PART_ID = 13;
+export const FIXED_PART_RUNE_2A_PART_ID = 14;
+export const FIXED_PART_RUNE_2B_PART_ID = 15;
+export const FIXED_PART_RUNE_2C_PART_ID = 16;
+export const FIXED_PART_RUNE_3A_PART_ID = 17;
+export const FIXED_PART_RUNE_3B_PART_ID = 18;
+export const FIXED_PART_RUNE_3C_PART_ID = 19;
+export const FIXED_PART_TREE_BLUE_PART_ID = 20;
+export const FIXED_PART_TREE_BRIGHT_PART_ID = 21;
+export const FIXED_PART_TREE_DARK_PART_ID = 22;
+
+export const SLOT_PART_PAGE_1_SLOT_ID = 1001;
+export const SLOT_PART_PAGE_2_SLOT_ID = 1002;
+export const SLOT_PART_PAGE_3_SLOT_ID = 1003;
+export const SLOT_PART_PAGE_4_SLOT_ID = 1004;
+export const SLOT_PART_PAGE_5_SLOT_ID = 1005;
+export const SLOT_PART_PAGE_6_SLOT_ID = 1006;
+export const SLOT_PART_PAGE_7_SLOT_ID = 1007;
+export const SLOT_PART_PAGE_8_SLOT_ID = 1008;
+export const SLOT_PART_PAGE_9_SLOT_ID = 1009;
+export const SLOT_PART_PAGE_10_SLOT_ID = 1010;
